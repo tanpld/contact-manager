b0bdc93bfd2f9c134c2a722d6c4f131ab2652b72
Tan Phan <duytan.phanle@gmail.com>
2019-02-15 23:14:00 +0700

    Updates

---
+++ precache-manifest.eacbdb41da95893721a819d8646c394b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "7b4423a8d4ef5edc91b1",
+    "url": "/contact-manager/static/js/main.7b4423a8.chunk.js"
+  },
+  {
+    "revision": "0d2167d8740ca051596e",
+    "url": "/contact-manager/static/js/1.0d2167d8.chunk.js"
+  },
+  {
+    "revision": "7b4423a8d4ef5edc91b1",
+    "url": "/contact-manager/static/css/main.76c04a9b.chunk.css"
+  },
+  {
+    "revision": "0d2167d8740ca051596e",
+    "url": "/contact-manager/static/css/1.1eb0cee4.chunk.css"
+  },
+  {
+    "revision": "6d3c3095835a071a268d88004f620990",
+    "url": "/contact-manager/index.html"
+  }
+];
